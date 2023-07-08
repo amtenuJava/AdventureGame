db085c5b4e997acb410cb52cb77da2292d02b184
amanuel <amannov21@gmail.com>
2023-07-08 08:48:55 -0600

    User can play the game

--- src/com/aman/AdventureGame.java
+++ src/com/aman/AdventureGame.java
@@ -4,65 +4,108 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.*;
 
 public class AdventureGame {
-    private static final String GAME_LOCATIONS= """
+    private static final String GAME_LOCATIONS = """
             road, at the end of the road,W: well house ,S:valley ,N:forest 
-            hill on top of the hill with a view to all directions ,N:forest ,E:road 
-            well house,inside a well house for a small spring ,W:road,N:Lake ,S:stream 
+            hill ,on top of the hill with a view to all directions ,N:forest ,E:road 
+            well, house inside a well house for a small spring ,W:road,N:Lake ,S:stream 
             valley,in a forest valley beside a tumbling stream,N:road,W:hill,E:Stream
-            
+                        
             """;
 
-    private enum Compass{
-        E,N,S,W;
+    private enum Compass {
+        E, N, S, W;
 
-        private static final String [] direction={
-                "East","North","South","West"
+        private static final String[] direction = {
+                "East", "North", "South", "West"
         };
 
-        public String getDirection(){
+        public String getString() {
             return direction[this.ordinal()];
         }
+
+
     }
 
-    private record Location(String description, Map<Compass,String> nextPlaces){}
+    private record Location(String description, Map<Compass, String> nextPlaces) {
+    }
 
     private String lastPlace;
 
-    private Map<String,Location> adventureMap=new HashMap<>();
+    private Map<String, Location> adventureMap = new HashMap<>();
 
-    public AdventureGame(){
-
-    }
-
-    public AdventureGame(String customeLocation) {
+    public AdventureGame() {
+        this(null);
 
     }
 
-    private void loadLocation(String data){
-        for(String s:data.split("\\R")){
-            String[] parts= s.split(",",3);
+    public AdventureGame(String customLocation) {
+        loadLocation(GAME_LOCATIONS);
+        if (customLocation != null) {
+            loadLocation(customLocation);//Custom location
+        }
+    }
+
+    private void loadLocation(String data) {
+        for (String s : data.split("\\R")) {
+            String[] parts = s.split(",", 3);
             Arrays.asList(parts).replaceAll(String::trim);
-            Map<Compass,String> nextPlaces=loadDirection(parts[2]);
-            Location location=new Location(parts[1],nextPlaces);
-            adventureMap.put(parts[0],location );
+            Map<Compass, String> nextPlaces = loadDirection(parts[2]);
+            Location location = new Location(parts[1], nextPlaces);
+            adventureMap.put(parts[0], location);
 
         }
-        adventureMap.forEach((k,v)-> System.out.printf("%s:%s%n",k,v));
+        //adventureMap.forEach((k, v) -> System.out.printf("%s:%s%n", k, v));
     }
 
     private Map<Compass, String> loadDirection(String nextPlaces) {
-        Map<Compass,String> directions=new HashMap<>();
-        List<String> nextSteps=Arrays.asList(nextPlaces.split(","));
+        Map<Compass, String> directions = new HashMap<>();
+        List<String> nextSteps = Arrays.asList(nextPlaces.split(","));
         nextSteps.replaceAll(String::trim);
-        for (String nextPlace:nextSteps){
-            String[] splits= nextPlace.split(":");
-            Compass compass=Compass.valueOf(splits[0].trim());
-            String destination=splits[1].trim();
-            directions.put(compass,destination);
+        for (String nextPlace : nextSteps) {
+            String[] splits = nextPlace.split(":");
+            Compass compass = Compass.valueOf(splits[0].trim());
+            String destination = splits[1].trim();
+            directions.put(compass, destination);
         }
         return directions;
+
+
+    }
+
+    private void visit(Location location) {
+        System.out.printf("*** You are standing %s ***%n", location.description);
+        System.out.println("From Here you can see:");
+        location.nextPlaces.forEach((k, v) -> {
+            System.out.printf("\t - A %s to the %s (%S) %n", v, k.getString(), k);
+        });
+        System.out.println("Select your compass (Q to quit) >> ");
+    }
+
+
+    public void move(String direction) {
+        var nextPlaces = adventureMap.get(lastPlace).nextPlaces;//next places map from the last place the player was
+        String nextPlace = null;
+        if ("ENSW".contains(direction)) {
+            nextPlace = nextPlaces.get(Compass.valueOf(direction));
+            if (nextPlace != null) {
+                play(nextPlace);
+            }
+        } else {
+            System.out.println("!! Invalid direction , try again");
+        }
+    }
+
+    public void play(String location) {
+        if (adventureMap.containsKey(location)) {
+             Location next=adventureMap.get(location);
+             lastPlace=location;
+             visit(next);
+        } else {
+            System.out.println(location + " Is not valid location");
+        }
 
 
     }
